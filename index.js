7fcebee96ca8c45720e80a427feb0f0e78fa2eb6
ariful892 <ariful00892@gmail.com>
2022-05-08 18:11:56 +0600

    deleting single item from database

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
             res.send(result);
         });
 
+        // delete item
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
